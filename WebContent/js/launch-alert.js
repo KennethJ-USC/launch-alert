1296e0bd4228b722621da19f1665e989e7a6da6d
kennethj-usc <kennethj@email.sc.edu>
2019-12-03 01:20:09 -0500

    added functionality w/ launch alert

--- WebContent/js/launch-alert.js
+++ WebContent/js/launch-alert.js
@@ -1,6 +1,9 @@
+
+var earth; 
+
 function initialize() {
     var options = {atmosphere: true, center: [0, 0], zoom: 0};
-    var earth = new WE.map('earth_div', options);
+    earth = new WE.map('earth_div', options);
     WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
         minZoom: 0,
         maxZoom: 5,
@@ -8,6 +11,8 @@ function initialize() {
     }).addTo(earth);
 }
 
+var llData;
 function load_ll_event() {
     launchAlertData();
+
 }
--- WebContent/js/loaddata.js
+++ WebContent/js/loaddata.js
@@ -4,11 +4,55 @@
 // Example: To return launches after August 20th, 2015:
 //      > https://launchlibrary.net/1.4/launch/2015-08-20
 
+var launches;
+var alaunch;
+
 function launchAlertData() {
     var oneWeekAgo = new Date();
+    var endDate = new Date();
+    
+    endDate.setDate(oneWeekAgo.getDate() + 182);
+    endDate = endDate.toISOString().substring(0,10);
+    
     oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+    oneWeekAgo = oneWeekAgo.toISOString().substring(0,10);
+    console.log(oneWeekAgo);  // >2019-11-26
     
-    var ll_rooturl = "https://launchlibrary.net/1.4/launch/";
+    var url = "";
+    var ll_rooturl = "https://launchlibrary.net/1.4/launch?";
     
-    console.log(oneWeekAgo);
+
+    url = ll_rooturl + "startdate=" + oneWeekAgo;
+    url = url + "&enddate=" + endDate;
+    url = url + "&limit=200";
+    url = url + "&mode=verbose";
+    console.log(url);
+    
+    fetch(url)
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) {
+        console.log(data);
+        
+        console.log(earth.toString());
+        console.log(url);
+        
+        launches = data.launches;
+        console.log("above for");
+        
+        alaunch = launches[1];
+        console.log(alaunch.location.pads[0].latitude);
+        for (var i=0; i<data.count; i++) {
+            var loc = launches[i].location.pads[0];
+            var lat = loc.latitude;
+            var lon = loc.longitude;
+            
+            var marker = new WE.marker([lat,lon]).addTo(earth);
+        }  // end for
+    })
+    .catch(function() {
+        
+    });
+
+    return launches;
+    
 }
